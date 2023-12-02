affa73a5797d0afc8d4076f200207b1895abb673
Ethan Bills <74191134+ejbills@users.noreply.github.com>
2023-12-02 15:30:29 -0800

    subscription drawer and local favs done (#2)

---
+++ OpenArtemis/Utilities/App Storage/PersistenceController.swift
@@ -0,0 +1,59 @@
+//
+//  PersistenceController.swift
+//  OpenArtemis
+//
+//  Created by Ethan Bills on 12/2/23.
+//
+
+import CoreData
+
+struct PersistenceController {
+    // A singleton for our entire app to use
+    static let shared = PersistenceController()
+
+    // Storage for Core Data
+    let container: NSPersistentContainer
+
+    // A test configuration for SwiftUI previews
+    static var preview: PersistenceController = {
+        let controller = PersistenceController(inMemory: true)
+
+        // Create 10 example programming languages.
+        for _ in 0..<10 {
+            let language = LocalSubreddit(context: controller.container.viewContext)
+            language.name = "Example Language 1"
+        }
+
+        return controller
+    }()
+
+    // An initializer to load Core Data, optionally able
+    // to use an in-memory store.
+    init(inMemory: Bool = false) {
+        // If you didn't name your model Main you'll need
+        // to change this name below.
+        container = NSPersistentContainer(name: "LocalData")
+
+        if inMemory {
+            container.persistentStoreDescriptions.first?.url = URL(fileURLWithPath: "/dev/null")
+        }
+
+        container.loadPersistentStores { description, error in
+            if let error = error {
+                fatalError("Error: \(error.localizedDescription)")
+            }
+        }
+    }
+    
+    func save() {
+        let context = container.viewContext
+
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                // Show some error here
+            }
+        }
+    }
+}
---
+++ OpenArtemis/Views/Misc/SectionIndexTitlesView.swift
@@ -0,0 +1,59 @@
+//
+//  SectionIndexTitlesView.swift
+//  OpenArtemis
+//
+//  Created by Ethan Bills on 12/2/23.
+//
+
+import SwiftUI
+
+struct SectionIndexTitlesView: View {
+    let proxy: ScrollViewProxy
+    let availChars: [String]
+    
+    @GestureState private var dragLocation: CGPoint = .zero
+    @State private var lastSelectedLabel: String = ""
+    
+    var body: some View {
+        VStack {
+            ForEach(availChars, id: \.self) { letter in
+                Text(letter)
+                    .bold()
+                    .frame(width: 25)
+                    .background(dragObserver(label: letter))
+            }
+        }
+        .background(
+            RoundedRectangle(cornerRadius: 6)
+                .foregroundColor(Color.gray.opacity(0.25))
+        )
+        .gesture(
+            DragGesture(minimumDistance: 0, coordinateSpace: .global)
+                .updating($dragLocation) { value, state, _ in
+                    state = value.location
+                }
+        )
+    }
+    
+    func dragObserver(label: String) -> some View {
+        GeometryReader { geometry in
+            dragObserver(geometry: geometry, label: label)
+        }
+    }
+    
+    func dragObserver(geometry: GeometryProxy, label: String) -> some View {
+        if geometry.frame(in: .global).contains(dragLocation) {
+            if label != lastSelectedLabel {
+                DispatchQueue.main.async {
+                    lastSelectedLabel = label
+                    proxy.scrollTo(label, anchor: .center)
+                    
+                    let impact = UIImpactFeedbackGenerator(style: .rigid)
+                    impact.impactOccurred()
+                }
+            }
+        }
+        return Rectangle().fill(Color.clear)
+    }
+}
+
