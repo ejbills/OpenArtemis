85719a66d21afe2fa26c12543edbfebd88f85887
Daniel <daniel.inama02@gmail.com>
2023-12-05 09:56:22 +0100

    comments and subs drawer design tweaks

--- OpenArtemis/Utilities/Post Utils/Comment Utils.swift
+++ OpenArtemis/Utilities/Post Utils/Comment Utils.swift
@@ -11,6 +11,7 @@ import SwiftUI
 struct Comment {
     let id: String
     let parentID: String?
+    var childID: String?
     let author: String
     let score: String // since comments show score as "X votes" rather than just the int
     let time: String
@@ -18,11 +19,32 @@ struct Comment {
     let depth: Int
     var isCollapsed: Bool
     var isRootCollapsed: Bool
+    
+    
+}
+
+func getNumberOfDescendants(for comment: Comment, in comments: [Comment]) -> Int {
+    return countDescendants(for: comment, in: comments)
+}
+
+private func countDescendants(for comment: Comment, in comments: [Comment]) -> Int {
+    // Filter comments that have the current comment as their parent
+    let children = comments.filter { $0.parentID == comment.id }
+    
+    // Initialize the count with the number of immediate children
+    var descendantCount = children.count
+
+    // Recursively count descendants for each child
+    for child in children {
+        descendantCount += countDescendants(for: child, in: comments)
+    }
+
+    // Return the total count of descendants
+    return descendantCount
 }
 
 public func commentIndentationColor(forDepth depth: Int) -> Color {
     // Choose a color based on depth
-    let colors: [Color] = [.red, .orange, .green, .blue, .purple, .pink]
-    let colorIndex = depth % colors.count
-    return colors[colorIndex]
+    let colorIndex = depth % colorPalette.count
+    return colorPalette[colorIndex]
 }
