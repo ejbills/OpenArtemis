0dff7dd42076e4c6cdb396cb9e066287f97d5376
Ethan Bills <74191134+ejbills@users.noreply.github.com>
2023-12-06 13:11:50 -0800

    fix crash out of range

--- OpenArtemis/Views/Subreddits/SubredditFeedView.swift
+++ OpenArtemis/Views/Subreddits/SubredditFeedView.swift
@@ -29,8 +29,10 @@ struct SubredditFeedView: View {
                                 .id(post.id)
                                 .contentShape(Rectangle())
                                 .onAppear {
-                                    if post.id == posts[Int(Double(posts.count) * 0.85)].id {
-                                        scrapeSubreddit(subredditName, lastPostAfter)
+                                    if !posts.isEmpty && posts.count > Int(Double(posts.count) * 0.85) {
+                                        if post.id == posts[Int(Double(posts.count) * 0.85)].id {
+                                            scrapeSubreddit(subredditName, lastPostAfter)
+                                        }
                                     }
                                 }
                                 .onTapGesture {
@@ -48,6 +50,7 @@ struct SubredditFeedView: View {
         }
         .id("\(subredditName)-feed-view")
         .navigationTitle((titleOverride != nil) ? titleOverride! : subredditName)
+        .navigationBarTitleDisplayMode(.inline)
         .onAppear {
             if posts.isEmpty {
                 scrapeSubreddit(subredditName)
