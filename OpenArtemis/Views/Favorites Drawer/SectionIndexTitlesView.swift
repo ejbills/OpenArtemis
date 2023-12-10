c0f8796c9f6310bc20420a4bf50c684784f00f5f
Ethan Bills <74191134+ejbills@users.noreply.github.com>
2023-12-09 17:17:52 -0800

    update fav drawer

--- OpenArtemis/Views/Favorites Drawer/SectionIndexTitlesView.swift
+++ OpenArtemis/Views/Favorites Drawer/SectionIndexTitlesView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct SectionIndexTitlesView: View {
     let proxy: ScrollViewProxy
     let availChars: [String]
-    
+
     @GestureState private var dragLocation: CGPoint = .zero
     @State private var lastSelectedLabel: String = ""
-    
+
     var body: some View {
         VStack {
             ForEach(availChars, id: \.self) { letter in
@@ -21,40 +21,48 @@ struct SectionIndexTitlesView: View {
                     .fontWeight(.medium)
                     .font(.system(size: 15))
                     .frame(width: 25)
+                    .offset(
+                        x: letter == lastSelectedLabel ? -50 : 0
+                    )
                     .background(dragObserver(label: letter))
             }
         }
         .background(
             RoundedRectangle(cornerRadius: 6)
-                .foregroundStyle(.ultraThinMaterial)
+                .foregroundStyle(tagBgColor)
         )
         .gesture(
             DragGesture(minimumDistance: 0, coordinateSpace: .global)
                 .updating($dragLocation) { value, state, _ in
                     state = value.location
                 }
+                .onEnded { value in
+                    withAnimation(.snappy(duration: 0.15)) {
+                        lastSelectedLabel = ""
+                    }
+                }
         )
     }
-    
+
     func dragObserver(label: String) -> some View {
         GeometryReader { geometry in
             dragObserver(geometry: geometry, label: label)
         }
     }
-    
+
     func dragObserver(geometry: GeometryProxy, label: String) -> some View {
         if geometry.frame(in: .global).contains(dragLocation) {
             if label != lastSelectedLabel {
                 DispatchQueue.main.async {
-                    lastSelectedLabel = label
-                    proxy.scrollTo(label, anchor: .center)
+                    withAnimation(.snappy(duration: 0.15)) {
+                        lastSelectedLabel = label
+                        HapticManager.shared.singleClick()
+                    }
                     
-                    let impact = UIImpactFeedbackGenerator(style: .rigid)
-                    impact.impactOccurred()
+                    proxy.scrollTo(label, anchor: .center)
                 }
             }
         }
         return Rectangle().fill(Color.clear)
     }
 }
-
