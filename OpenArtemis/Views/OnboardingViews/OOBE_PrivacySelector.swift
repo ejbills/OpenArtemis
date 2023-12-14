71a3fdcdb781a540d1820f572188b5c2b7425ed1
Ethan Bills <74191134+ejbills@users.noreply.github.com>
2023-12-14 11:22:55 -0800

    Update OOBE_PrivacySelector.swift

--- OpenArtemis/Views/OnboardingViews/OOBE_PrivacySelector.swift
+++ OpenArtemis/Views/OnboardingViews/OOBE_PrivacySelector.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct OOBE_PrivacySelector: View {
     var body: some View {
-        Text("Opt for OpenArtemis to guide you to more private website alternatives. You can also activate automatic removal of tracking parameters from all links.")
+        Text("Choose OpenArtemis to guide you to more private website alternatives. You can also enable the automatic removal of tracking parameters from all links.")
             .padding(.top, 8)
             .padding(.horizontal)
             .opacity(0.8)
