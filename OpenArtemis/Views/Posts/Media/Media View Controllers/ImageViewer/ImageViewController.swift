cac06820952c480e341017a687c21e0cf3caf280
Ethan Bills <74191134+ejbills@users.noreply.github.com>
2024-01-04 13:10:17 -0800

    image scroll indicator

--- OpenArtemis/Views/Posts/Media/Media View Controllers/ImageViewer/ImageViewController.swift
+++ OpenArtemis/Views/Posts/Media/Media View Controllers/ImageViewer/ImageViewController.swift
@@ -51,7 +51,7 @@ private struct ImageView: View {
     
     @State private var offset: CGSize = .zero
     @State private var index: Int = 0
-
+    
     var body: some View {
         LazyPager(data: images, page: $index) { url in
             let url = URL(string: url)
@@ -59,7 +59,7 @@ private struct ImageView: View {
             CachedImage(
                 url: url,
                 content: { image in
-                   LiveTextInteraction(image: image)
+                    LiveTextInteraction(image: image)
                         .scaledToFit()
                 },
                 placeholder: {
@@ -72,15 +72,56 @@ private struct ImageView: View {
         .onDismiss {
             dismissView()
         }
+        .ignoresSafeArea()
+        .overlay(
+            images.count > 1 ?
+            VanillaPageControl(numberOfPages: images.count, currentPage: $index)
+                .padding(.bottom, 20)
+                .allowsHitTesting(false) : nil,
+            alignment: .bottom
+        )
     }
-
+    
     private func dismissView() {
         if let rootViewClosure = rootViewClosure {
             rootViewClosure()
         }
-
+        
         withAnimation(.spring()) {
             offset = .zero
+        }
+    }
+}
+
+struct VanillaPageControl: UIViewRepresentable {
+    var numberOfPages: Int
+    @Binding var currentPage: Int
+
+    func makeUIView(context: Context) -> UIPageControl {
+        let pageControl = UIPageControl()
+        pageControl.numberOfPages = numberOfPages
+        pageControl.currentPage = currentPage
+        pageControl.addTarget(context.coordinator, action: #selector(Coordinator.updateCurrentPage(sender:)), for: .valueChanged)
+        return pageControl
+    }
+
+    func updateUIView(_ uiView: UIPageControl, context: Context) {
+        uiView.currentPage = currentPage
+    }
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    class Coordinator: NSObject {
+        var parent: VanillaPageControl
+
+        init(_ pageControl: VanillaPageControl) {
+            self.parent = pageControl
+        }
+
+        @objc func updateCurrentPage(sender: UIPageControl) {
+            parent.currentPage = sender.currentPage
         }
     }
 }
